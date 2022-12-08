420ca0cdbec585ed717cabedb4cb4a2e52f715e9
Youssef-18 <saoudiyoussef59@gmail.com>
2022-12-08 11:42:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import data from'./data';
+import Animalcard from '../Animalcard/Animalcard';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Animals</h1>
+      {data.map(animal => (
+        <AnimalCard key={animal.name}/>
+      ))}
     </div>
-  );
+  ) 
 }
 
 export default App;
